31e6c4c3a1443aa0394df188ea1f94c8e863bce2
Phoenix Zhang <pho.coderzhang@gmail.com>
2016-06-12 09:42:17 +0800

    init

---
+++ magpie-client-clj/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to magpie-client-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ magpie-client-clj/project.clj
@@ -0,0 +1,12 @@
+(defproject magpie-client-clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.slf4j/slf4j-log4j12 "1.7.21"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.apache.thrift/libthrift "0.9.1"]
+                 [clj-zookeeper "0.2.0-SNAPSHOT"]
+                 [com.jd.bdp.magpie/magpie-utils "0.1.3-SNAPSHOT"]]
+  :main ^:skip-aot com.jd.bdp.magpie.client)
